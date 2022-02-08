ca2132d0f37a02337b1d5ac58297e7c5c793082c
Raghavendra Shet <Raghavendra Shet@LAPTOP-UPO9JUJS>
2022-02-08 17:20:27 +0530

    Initial commit

---
+++ MyBankApp/src/main/java/com/raghavrs/bankapp/BankAppUseCaseApplication.java
@@ -0,0 +1,16 @@
+package com.raghavrs.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BankAppUseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppUseCaseApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
